8dee226ee44b496d0b853546a8a3be26111630e3
Fynzie <fynz@hotmail.fr>
2018-11-28 23:56:18 +0100

    Added some endpoints

---
+++ src/SocialCrunch/Controllers/ServiceController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialCrunch.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}")]
+    [ApiVersion("2")]
+    public class ServiceController : Controller
+    {
+        /// <summary>
+        /// I can even describe what the endpoint does
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("start")]
+        public IActionResult StartService()
+        {
+            return Json(new {PlaceHolder = "Not Yet Implemented"});
+        }
+
+        /// <summary>
+        /// Such amazing
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("stop")]
+        public IActionResult StopService()
+        {
+            return Json(new { PlaceHolder = "Not Yet Implemented" });
+        }
+
+        /// <summary>
+        /// Much wow
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet("user/run/{id}")]
+        public IActionResult RunForUser(int userId)
+        {
+            return Json(new { PlaceHolder = "Not Yet Implemented" });
+        }
+    }
+}
